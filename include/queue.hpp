cf93fb7244075cf06ecd4265b89e7679ff10e4d4
Bonington <yikun.chen@163.com>
2022-12-10 21:02:31 +0800

    更新代码范式：Allman

--- include/queue.hpp
+++ include/queue.hpp
@@ -9,49 +9,54 @@
 
 #include "bonostlpch.h"
 
-namespace Bonostl {
+namespace Bonostl
+{
 
     template<typename T>
-    class queue {
+    class queue
+    {
     private:
-        struct node {
+        struct node
+        {
             std::shared_ptr<T> data;
             std::unique_ptr<node> next;
 
             explicit node(T i_data)
-                    : data(std::move(i_data)) {}
+                    : data( std::move(i_data) ) {}
         };
 
         std::unique_ptr<node> head;
-        node *tail;
+        node* tail;
 
     public:
-        queue() : head(new node), tail(head.get()) {}
+        queue() : head(new node), tail( head.get() ) {}
 
-        queue(const queue &other) = delete;
+        queue(const queue& other) = delete;
 
-        queue &operator=(const queue &other) = delete;
+        queue &operator=(const queue& other) = delete;
 
-        std::shared_ptr<T> try_pop() {
-            if (head.get() == tail)
+        std::shared_ptr<T> try_pop()
+        {
+            if ( head.get() == tail )
                 return std::shared_ptr<T>();
 
-            std::shared_ptr<T> const res(head->data);
+            std::shared_ptr<T> const res(head -> data);
             std::unique_ptr<node> const old_head = std::move(head);
-            head = std::move(old_head->next);
+            head = std::move(old_head -> next);
 
             return res;
         }
 
-        void push(T new_value) {
+        void push(T new_value)
+        {
             std::shared_ptr<T> new_data(
-                    std::make_shared<T>(std::move(new_value))
+                    std::make_shared<T>( std::move(new_value) )
             );
             std::unique_ptr<node> p(new node);
-            tail->data = new_data;
+            tail -> data = new_data;
 
-            node *const new_tail = p.get();
-            tail->next = std::move(p);
+            node* const new_tail = p.get();
+            tail -> next = std::move(p);
 
             tail = new_tail;
         }
