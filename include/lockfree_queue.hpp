a98c4fe24cf8ecce4fa329dd7460472362cf5009
Bonington <yikun.chen@163.com>
2022-12-13 15:13:47 +0800

    加入数据结构：基于原子操作的无锁队列

--- include/lockfree_queue.hpp
+++ include/lockfree_queue.hpp
@@ -34,7 +34,7 @@ namespace Bonostl
         {
             std::atomic<T*> data;
             std::atomic<node_counter> count;
-            counted_node_ptr next;
+            std::atomic<counted_node_ptr> next;
 
             node()
             {
@@ -143,16 +143,31 @@ namespace Bonostl
             }
         }
         
-        std::shared_ptr<T> pop()
+        std::unique_ptr<T> pop()
         {
-            node* old_head = pop_head();
-            
-            if (!old_head)
-                return std::shared_ptr<T>();
-            
-            std::shared_ptr<T> const res(old_head -> data);
-            delete old_head;
-            return res;
+            counted_node_ptr old_head = head.load(std::memory_order_relaxed);
+
+            for (;;)
+            {
+                increase_external_count(head, old_head);
+                node* const ptr = old_head.ptr;
+
+                if ( ptr == tail.load().ptr )
+                {
+                    return std::unique_ptr<T>();
+                }
+                counted_node_ptr next = ptr -> next.load();
+
+                if ( head.compare_exchange_strong(old_head, next) )
+                {
+                    T* const res = ptr -> data.exchange(nullptr);
+                    free_external_counter(old_head);
+
+                    return std::unique_ptr<T>(res);
+                }
+
+                ptr -> release_ref();
+            }
         }
         
         void push(T new_value)
