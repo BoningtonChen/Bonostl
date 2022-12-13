74c563c2aefcd58e636375ef15db07782f2a86d8
Bonington <yikun.chen@163.com>
2022-12-13 15:48:44 +0800

    完成构建数据结构：基于原子操作的无锁队列

--- include/lockfree_queue.hpp
+++ include/lockfree_queue.hpp
@@ -125,6 +125,23 @@ namespace Bonostl
             if ( !new_counter.internal_count && !new_counter.external_counters )
                 delete ptr;
         }
+
+        void set_new_tail(
+                counted_node_ptr &old_tail,
+                counted_node_ptr const &new_tail
+                )
+        {
+            node* const current_tail_ptr = old_tail.ptr;
+
+            while (
+                    !tail.compare_exchange_weak(old_tail, new_tail)
+                    && old_tail.ptr == current_tail_ptr
+                    );
+            if ( old_tail.ptr == current_tail_ptr )
+                free_external_counter(old_tail);
+            else
+                current_tail_ptr -> release_ref();
+        }
         
     public:
         lockfree_queue()
@@ -190,16 +207,38 @@ namespace Bonostl
                                 )
                         )
                 {
-                    old_tail.ptr -> next = new_next;
-                    old_tail = tail.exchange(new_next);
+                    counted_node_ptr old_next = {0};
 
-                    free_external_counter(old_tail);
+                    if (
+                            !old_tail.ptr -> next.compare_exchange_strong(
+                                    old_next, new_next
+                                    )
+                            )
+                    {
+                        delete new_next.ptr;
+                        new_next = old_next;
+                    }
 
+                    set_new_tail(old_tail, new_next);
                     new_data.release();
                     break;
                 }
+                else
+                {
+                    counted_node_ptr old_next = {0};
 
-                old_tail.ptr -> release_ref();
+                    if (
+                            old_tail.ptr -> next.compare_exchange_strong(
+                                    old_next, new_next
+                                    )
+                            )
+                    {
+                        old_next = new_next;
+                        new_next.ptr = new node;
+                    }
+
+                    set_new_tail(old_tail, old_next);
+                }
             }
         }
     };
