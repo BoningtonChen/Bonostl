7b3c3df208dee58e404f0501e1a44df1dd4c551e
Clément CHEN <yikun.chen@163.com>
2026-08-10 23:02:28 +0800

    refactor: fix threadsafe_stack/queue bugs, return optional from pop

--- include/threadsafe_queue.hpp
+++ include/threadsafe_queue.hpp
@@ -1,124 +1,39 @@
-//
-// Created by 陈奕锟 on 2022/12/3.
-//
-
-#ifndef BONOSTL_THREADSAFE_QUEUE_HPP
-#define BONOSTL_THREADSAFE_QUEUE_HPP
-
-#endif //BONOSTL_THREADSAFE_QUEUE_HPP
 #pragma once
 
 #include "bonostlpch.h"
 
 namespace Bonostl
 {
-
     template<typename T>
     class threadsafe_queue
     {
-    private:
-        struct node
-        {
-            std::shared_ptr<T> data;
-            std::unique_ptr<node> next;
-        };
-        std::mutex head_mutex;
-        std::unique_ptr<node> head;
-        std::mutex tail_mutex;
-        node* tail;
-
-        std::condition_variable data_cond;
-
-        node* get_tail()
-        {
-            std::lock_guard<T> tail_lock(tail_mutex);
-            return tail;
-        }
-
-        std::unique_ptr<node> pop_head()
-        {
-            std::unique_ptr<node> old_head = std::move(head);
-            head = std::move( old_head -> next );
-
-            return old_head;
-        }
-
-        std::unique_lock<std::mutex> wait_for_data()
-        {
-            std::unique_lock<T> head_lock(head_mutex);
-            data_cond.wait(head_lock, [&] { return head.get() != get_tail(); });
-
-            return std::move(head_lock);
-        }
-
-        std::unique_ptr<node> wait_pop_head()
-        {
-            std::unique_lock<T> head_lock(wait_for_data());
-            return pop_head();
-        }
-
-        std::unique_ptr<node> wait_pop_head(T& value)
-        {
-            std::unique_lock<T> head_lock( wait_for_data() );
-            value = std::move( *head -> data);
-
-            return pop_head();
-        }
-
-        std::unique_ptr<node> try_pop_head()
-        {
-            std::lock_guard<T> head_lock(head_mutex);
-
-            if (head.get() == get_tail())
-                return std::unique_ptr<node>();
-
-            return pop_head();
-        }
-
-        std::unique_ptr<node> try_pop_head(T& value)
-        {
-            std::lock_guard<T> head_lock(head_mutex);
-
-            if ( head.get() == get_tail() )
-                return std::unique_ptr<node>();
-
-            value = std::move( *head -> data);
-
-            return pop_head();
-        }
-
     public:
         threadsafe_queue()
-            : head(new node), tail( head.get() )
-        {}
+            : head(std::make_unique<node>()), tail(head.get())
+        {
+        }
 
-        threadsafe_queue(const threadsafe_queue& other) = delete;
-
-        threadsafe_queue &operator=(const threadsafe_queue& other) = delete;
+        threadsafe_queue(const threadsafe_queue&) = delete;
+        threadsafe_queue& operator=(const threadsafe_queue&) = delete;
 
         void push(T new_value)
         {
-            std::shared_ptr<T> new_data(
-                    std::make_shared<T>(std::move(new_value))
-            );
-
+            auto new_tail = std::make_unique<node>();
             {
-                std::unique_ptr<node> p(new node);
-                std::lock_guard<T> tail_lock(tail_mutex);
+                std::lock_guard tail_lock(tail_mutex_);
 
-                tail -> data = new_data;
-                node* const new_tail = p.get();
-                tail -> next = std::move(p);
-                tail = new_tail;
+                tail->data = std::move(new_value);
+                node* const next_tail = new_tail.get();
+                tail->next = std::move(new_tail);
+                tail = next_tail;
             }
-            data_cond.notify_one();
+            data_cond_.notify_one();
         }
 
-        std::shared_ptr<T> wait_and_pop()
+        T wait_and_pop()
         {
-            std::unique_ptr<node> const old_head = wait_and_pop();
-
-            return old_head -> data;
+            std::unique_ptr<node> const old_head = wait_pop_head();
+            return std::move(*old_head->data);
         }
 
         void wait_and_pop(T& value)
@@ -126,25 +41,97 @@ namespace Bonostl
             std::unique_ptr<node> const old_head = wait_pop_head(value);
         }
 
-        std::shared_ptr<T> try_pop()
+        std::optional<T> try_pop()
         {
-            std::unique_ptr<node> old_head = pop_head();
+            std::unique_ptr<node> old_head = try_pop_head();
 
-            return old_head ? old_head -> data : std::shared_ptr<T>();
+            return old_head ? std::optional<T>(std::move(*old_head->data)) : std::nullopt;
         }
 
         bool try_pop(T& value)
         {
             std::unique_ptr<node> const old_head = try_pop_head(value);
+            return old_head != nullptr;
+        }
+
+        bool empty() const
+        {
+            std::lock_guard head_lock(head_mutex_);
+            return head.get() == get_tail();
+        }
+
+    private:
+        struct node
+        {
+            std::optional<T> data;
+            std::unique_ptr<node> next;
+        };
+
+        std::mutex head_mutex_;
+        std::unique_ptr<node> head;
+        std::mutex tail_mutex_;
+        node* tail;
+        std::condition_variable data_cond_;
+
+        node* get_tail()
+        {
+            std::lock_guard tail_lock(tail_mutex_);
+            return tail;
+        }
+
+        std::unique_ptr<node> pop_head()
+        {
+            std::unique_ptr<node> old_head = std::move(head);
+            head = std::move(old_head->next);
+
             return old_head;
         }
 
-        bool empty()
+        std::unique_lock<std::mutex> wait_for_data()
         {
-            std::lock_guard<T> head_lock(head_mutex);
+            std::unique_lock<std::mutex> head_lock(head_mutex_);
+            data_cond_.wait(head_lock, [&] { return head.get() != get_tail(); });
 
-            return head.get() == get_tail();
+            return head_lock;
+        }
+
+        std::unique_ptr<node> wait_pop_head()
+        {
+            std::unique_lock<std::mutex> head_lock(wait_for_data());
+            return pop_head();
+        }
+
+        std::unique_ptr<node> wait_pop_head(T& value)
+        {
+            std::unique_lock<std::mutex> head_lock(wait_for_data());
+            value = std::move(*head->data);
+
+            return pop_head();
+        }
+
+        std::unique_ptr<node> try_pop_head()
+        {
+            std::lock_guard head_lock(head_mutex_);
+
+            if (head.get() == get_tail())
+            {
+                return nullptr;
+            }
+
+            return pop_head();
+        }
+
+        std::unique_ptr<node> try_pop_head(T& value)
+        {
+            std::lock_guard head_lock(head_mutex_);
+
+            if (head.get() == get_tail())
+            {
+                return nullptr;
+            }
+
+            value = std::move(*head->data);
+            return pop_head();
         }
     };
-
 }
--- include/threadsafe_stack.hpp
+++ include/threadsafe_stack.hpp
@@ -1,81 +1,71 @@
-//
-// Created by 陈奕锟 on 2022/12/3.
-//
-
-#ifndef BONOSTL_THREADSAFE_STACK_HPP
-#define BONOSTL_THREADSAFE_STACK_HPP
-
-#endif //BONOSTL_THREADSAFE_STACK_HPP
 #pragma once
 
 #include "bonostlpch.h"
 
 namespace Bonostl
 {
-
-    struct empty_stack : std::exception
-    {
-        [[nodiscard]] const char *what() const noexcept override;
-    };
-
     template<typename T>
     class threadsafe_stack
     {
-    private:
-        std::stack<T> data;
-        mutable std::mutex m;
-
     public:
         threadsafe_stack() = default;
 
         threadsafe_stack(const threadsafe_stack& other)
         {
-            std::lock_guard<std::mutex> lock(other.m);
-
-            data = other.data;
+            std::lock_guard lock(other.mutex_);
+            data_ = other.data_;
         }
 
-        threadsafe_stack& operator=(const threadsafe_stack& ) = delete;
+        threadsafe_stack(threadsafe_stack&& other) noexcept
+            : data_(std::move(other.data_))
+        {
+        }
+
+        threadsafe_stack& operator=(const threadsafe_stack&) = delete;
+        threadsafe_stack& operator=(threadsafe_stack&&) = delete;
 
         void push(T new_value)
         {
-            std::lock_guard<T> lock(m);
-
-            data.push( std::move(new_value) );
+            std::lock_guard lock(mutex_);
+            data_.push(std::move(new_value));
         }
 
-        std::shared_ptr<T> pop()
+        std::optional<T> pop()
         {
-            std::lock_guard<T> lock(m);
+            std::lock_guard lock(mutex_);
 
-            if (data.empty()) throw empty_stack();
+            if (data_.empty())
+            {
+                return std::nullopt;
+            }
 
-            std::shared_ptr<T> const res(
-                    std::make_shared<T>( std::move(data.top()) )
-            );
-
-            data.pop();
-
-            return res;
+            std::optional<T> result = std::move(data_.top());
+            data_.pop();
+            return result;
         }
 
-        void pop(T& value)
+        bool try_pop(T& value)
         {
-            std::lock_guard<T> lock(m);
+            std::lock_guard lock(mutex_);
 
-            if (data.empty()) throw empty_stack();
+            if (data_.empty())
+            {
+                return false;
+            }
 
-            value = std::move(data.top());
-            data.pop();
+            value = std::move(data_.top());
+            data_.pop();
+            return true;
         }
 
         bool empty() const
         {
-            std::lock_guard<T> lock(m);
-
-            return data.empty();
+            std::lock_guard lock(mutex_);
+            return data_.empty();
         }
 
+    private:
+        std::stack<T> data_;
+        mutable std::mutex mutex_;
     };
-
 }
