3f67867b6d374e13ac13e820aac25b3bf8954460
Clément CHEN <yikun.chen@163.com>
2026-08-10 23:07:01 +0800

    refactor: fix threadsafe_list node type and destructor predicate

--- include/threadsafe_list.hpp
+++ include/threadsafe_list.hpp
@@ -1,11 +1,3 @@
-//
-// Created by 陈奕锟 on 2022/12/12.
-//
-
-#ifndef BONOSTL_THREADSAFE_LIST_HPP
-#define BONOSTL_THREADSAFE_LIST_HPP
-
-#endif //BONOSTL_THREADSAFE_LIST_HPP
 #pragma once
 
 #include "bonostlpch.h"
@@ -20,34 +12,35 @@ namespace Bonostl
         {
             std::mutex m;
             std::shared_ptr<T> data;
-            std::unique_ptr<T> next;
+            std::unique_ptr<node> next;
 
-            node()
-                    : next()
-            {}
-            explicit node(T const& value)
-                    : data( std::make_shared<T>(value) )
-            {}
+            node() = default;
+
+            explicit node(T value)
+                : data(std::make_shared<T>(std::move(value)))
+            {
+            }
         };
 
         node head;
 
     public:
         threadsafe_list() = default;
+
         ~threadsafe_list()
         {
-            remove_if( [](node const&) { return true; } );
+            remove_if([](T const&) { return true; });
         }
 
-        threadsafe_list(threadsafe_list const& other) = delete;
-        threadsafe_list& operator=(threadsafe_list const& other) = delete;
+        threadsafe_list(const threadsafe_list&) = delete;
+        threadsafe_list& operator=(const threadsafe_list&) = delete;
 
-        void push_front(T const& value)
+        void push_front(T value)
         {
-            std::unique_ptr<node> new_node( new node(value) );
-            std::lock_guard<std::mutex> lk(head.m);
+            std::unique_ptr<node> new_node = std::make_unique<node>(std::move(value));
+            std::lock_guard lock(head.m);
 
-            new_node -> next = std::move(head.next);
+            new_node->next = std::move(head.next);
             head.next = std::move(new_node);
         }
 
@@ -57,12 +50,12 @@ namespace Bonostl
             node* current = &head;
             std::unique_lock<std::mutex> lk(head.m);
 
-            while ( node* const next = current -> next.get() )
+            while (node* const next = current->next.get())
             {
-                std::unique_lock<std::mutex> next_lk(next -> m);
+                std::unique_lock<std::mutex> next_lk(next->m);
                 lk.unlock();
 
-                func(*next -> data);
+                func(*next->data);
                 current = next;
                 lk = std::move(next_lk);
             }
@@ -74,21 +67,21 @@ namespace Bonostl
             node* current = &head;
             std::unique_lock<std::mutex> lk(head.m);
 
-            while ( node* const next = current -> next.get() )
+            while (node* const next = current->next.get())
             {
-                std::unique_lock<std::mutex> next_lk(next -> m);
+                std::unique_lock<std::mutex> next_lk(next->m);
                 lk.unlock();
 
-                if ( p(*next -> data) )
+                if (p(*next->data))
                 {
-                    return next -> data;
+                    return next->data;
                 }
 
                 current = next;
                 lk = std::move(next_lk);
             }
 
-            return std::shared_ptr<T>();
+            return {};
         }
 
         template<typename Predicate>
@@ -97,21 +90,20 @@ namespace Bonostl
             node* current = &head;
             std::unique_lock<std::mutex> lk(head.m);
 
-            while ( node* const next = current -> next.get() )
+            while (node* const next = current->next.get())
             {
-                std::unique_lock<std::mutex> next_lk(next -> m);
+                std::unique_lock<std::mutex> next_lk(next->m);
 
-                if ( p(*next -> data) )
+                if (p(*next->data))
                 {
-                    std::unique_ptr<node> old_next = std::move(current -> next);
-                    current -> next = std::move(next -> next);
+                    std::unique_ptr<node> old_next = std::move(current->next);
+                    current->next = std::move(next->next);
                     next_lk.unlock();
                 }
                 else
                 {
                     lk.unlock();
                     current = next;
-
                     lk = std::move(next_lk);
                 }
             }
