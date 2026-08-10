565b2106ad734663b9ec7a6a8a59b0afe74d67ea
Clément CHEN <yikun.chen@163.com>
2026-08-10 23:00:51 +0800

    refactor: modernize spinlock, join_threads and queue with C++26 idioms

--- include/join_threads.hpp
+++ include/join_threads.hpp
@@ -1,11 +1,3 @@
-//
-// Created by 陈奕锟 on 2022/12/13.
-//
-
-#ifndef BONOSTL_JOIN_THREADS_HPP
-#define BONOSTL_JOIN_THREADS_HPP
-
-#endif //BONOSTL_JOIN_THREADS_HPP
 #pragma once
 
 #include "bonostlpch.h"
@@ -14,20 +6,27 @@ namespace Bonostl
 {
     class join_threads
     {
-    private:
-        std::vector<std::thread>& threads;
-
     public:
-        explicit join_threads( std::vector<std::thread>& i_threads )
-                : threads(i_threads)
-        {}
+        explicit join_threads(std::vector<std::thread>& threads) noexcept
+            : threads_(threads)
+        {
+        }
+
+        join_threads(const join_threads&) = delete;
+        join_threads& operator=(const join_threads&) = delete;
+
         ~join_threads()
         {
-            for (auto& thread : threads)
+            for (auto& thread : threads_)
             {
-                if ( thread.joinable() )
+                if (thread.joinable())
+                {
                     thread.join();
+                }
             }
         }
+
+    private:
+        std::vector<std::thread>& threads_;
     };
 }
--- include/spinlock_mutex.hpp
+++ include/spinlock_mutex.hpp
@@ -1,36 +1,39 @@
-//
-// Created by 陈奕锟 on 2022/12/3.
-//
-
-#ifndef BONOSTL_SPINLOCK_MUTEX_HPP
-#define BONOSTL_SPINLOCK_MUTEX_HPP
-
-#endif //BONOSTL_SPINLOCK_MUTEX_HPP
 #pragma once
 
 #include "bonostlpch.h"
 
 namespace Bonostl
 {
-
     class spinlock_mutex
     {
+    public:
+        spinlock_mutex() noexcept = default;
+
+        spinlock_mutex(const spinlock_mutex&) = delete;
+        spinlock_mutex& operator=(const spinlock_mutex&) = delete;
+
+        void lock() noexcept
+        {
+            while (flag.test_and_set(std::memory_order_acquire))
+            {
+                while (flag.test(std::memory_order_relaxed))
+                {
+                    std::this_thread::yield();
+                }
+            }
+        }
+
+        [[nodiscard]] bool try_lock() noexcept
+        {
+            return !flag.test_and_set(std::memory_order_acquire);
+        }
+
+        void unlock() noexcept
+        {
+            flag.clear(std::memory_order_release);
+        }
+
     private:
         std::atomic_flag flag;
-
-    public:
-        spinlock_mutex()
-                : flag(ATOMIC_FLAG_INIT) {}
-
-        void lock()
-        {
-            while ( flag.test_and_set(std::memory_order_acquire) );
-        }
-
-        void unlock()
-        {
-            flag.clear(std::memory_order_relaxed);
-        }
     };
-
 }
